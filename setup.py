4a03eb0eadb21a9f7776ccf136b6fcfeafa97abd
Hrithik Patel <hrithikpatel200@gmail.com>
2022-10-30 00:37:31 +0530

    CICD GIT

--- setup.py
+++ setup.py
@@ -1,33 +1,38 @@
-from setuptools import setup,find_packages 
+from setuptools import setup,find_packages
 from typing import List
 
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.3"
+AUTHOR="Avnish Yadav"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 
-#Declaring variables for setup
+REQUIREMENT_FILE_NAME="requirements.txt"
 
-project_name="housing prediction"
-author="Hrithik Patel"
-version="0.0.5"
-license="Apache"
-requirement_file='requirements.txt'
+HYPHEN_E_DOT = "-e ."
 
-def get_requirement()->List[str]:
 
-    """"
-    This function is going to return a list of libraries mention  
-    in requirements.txt file
+def get_requirements_list() -> List[str]:
     """
-    with open(requirement_file,'r') as requirements:
-         return requirements.readlines().remove("-e .")
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
 
 
 setup(
-    project_name=project_name,
-    author=author,
-    version=version,
-    packages=find_packages(),
-    license=license,
-    install_requires=get_requirement()
-
-
-
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
 )
